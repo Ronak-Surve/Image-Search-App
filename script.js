1c2d48eadc8485153d2366e59c0ee5795243e8e1
Ronak-Surve <ronaksurve344rs@gmail.com>
2024-09-07 19:26:48 +0530

    removed .env and added secretFile in .gitignore

---
+++ script.js
@@ -0,0 +1,58 @@
+const dotenv = require('dotenv').config();
+
+const formElement = document.querySelector("form");
+const inputElement = document.querySelector("input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputSearch = "";
+let page = 1;
+
+async function searchImages() {
+
+    inputSearch = inputElement.value;
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputSearch}&client_id=${process.env.ACCESS_KEY}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = await data.results;
+
+    if(page === 1)  {
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) => {
+
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+
+        page++;
+        if(page > 1)    {
+            showMore.style.display = "block";
+        }
+    })
+}
+
+formElement.addEventListener('submit', (e) =>   {
+
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener('click', (e) =>   {
+    searchImages();
+});
